13b4feb89816f9e69e44fd028a1aece114abe88f
ashiqur.superfly <ashiqur.buet16@gmail.com>
2020-11-19 12:33:54 +0600

    refactor

--- app/src/main/java/com/ashiqurrahman/MainActivity.kt
+++ app/src/main/java/com/ashiqurrahman/MainActivity.kt
@@ -35,18 +35,17 @@ class MainActivity : AppCompatActivity() {
         VidChatConfig.CustomButton.customBtnListener = View.OnClickListener {
             Toast.makeText(this, "Clicked Custom button", Toast.LENGTH_LONG).show()
         }
-/*
+        /*
+            val customTURNSTUNConfig = CustomTURNSTUNConfig(
+                turnServerUrl = "turn:103.147.168.11:3478",
+                stunServerUrl = "stun:103.147.168.11:3478",
+                username = "blah",
+                credential = "blah123"
+            )
 
-        val customTURNSTUNConfig = CustomTURNSTUNConfig(
-            turnServerUrl = "turn:103.147.168.11:3478",
-            stunServerUrl = "stun:103.147.168.11:3478",
-            username = "blah",
-            credential = "blah123"
-        )
-
-        VidChatConfig.customRoomServerUrl = "https://mycustomroomserverurl"
-        VidChatConfig.customTURNSTUNConfig = customTURNSTUNConfig
-*/
+            VidChatConfig.customRoomServerUrl = "https://mycustomroomserverurl"
+            VidChatConfig.customTURNSTUNConfig = customTURNSTUNConfig
+        */
 
         VidChat.requestVideoChatPermissions(this, PERMISSION_REQUEST_CODE)
 
